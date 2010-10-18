be1a3a7c172b9bb5e52ff85e149c683c336c4b80
gsmsteve <gsmsteve@d92c909e-a44a-ba51-b3a1-27d943522830>
2010-10-18 04:54:42 +0000

    Added the hglibrary. This library is named after the initials of my wife and my last name. It's main purpose is to collect algorithms, design patterns and useful programming best practices in the hope that it will be useful and valuable for myself and others who want to use it.

---
+++ fall-2010/hglib/config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "hglib"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "gsmsteve@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "hglib"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "hglib 0.02"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "hglib"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.02"
+
+/* Version number of package */
+#define VERSION "0.02"
