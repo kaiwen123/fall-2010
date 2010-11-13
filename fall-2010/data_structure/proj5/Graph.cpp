1dee1f5bd9f246a5a9fa2339a155395f4ed11fa3
gsmsteve <gsmsteve@d92c909e-a44a-ba51-b3a1-27d943522830>
2010-11-13 02:49:15 +0000

    Changes to project 5 of data structure.

--- fall-2010/data_structure/proj5/Graph.cpp
+++ fall-2010/data_structure/proj5/Graph.cpp
@@ -7,151 +7,93 @@
  */
 // $Log$
 
-#include "Employee.h"
+#include "Graph.h"
 
-Employee::Employee() {
-  cout << "Please enter last name(string): "; 
-  cin >> last_name; 
-  if((last_name.at(0)<='z') && (last_name.at(0)>='a')) 
-    last_name.at(0) = last_name.at(0) - ('a' - 'A');
-  cout << "Please enter first name(string): "; 
-  cin >> first_name; 
-  cout << "Please enter department(string): "; 
-  cin >> department; 
-  cout << "Please enter phone number(string): "; 
-  cin >> phone_num; 
-  cout << "Please enter office building address(string): "; 
-  cin >> office_building; 
-  cout << "Please enter office room number(string): "; 
-  cin >> office_num; 
-  cout << "Please enter hire date(string): "; 
-  cin >> hire_date; 
-  cout << "Please enter email(string): ";
-  cin >> email; 
-
-  cout << "Please enter employee ID(int): ";
-  while (!(cin >> eid) || cin.get() != '\n') {
-    cin.clear();
-    cin.ignore(1000, '\n');
-    cout << "Incorrect entry! Please enter a number for EID: ";
+// Load graph structure data from file.
+bool Graph::loadFileToAdjList(string fname) {
+  int num; 			// number of vertices.
+  int w; 			// weight between vertices. 
+  ifstream fdata(fname.c_str()); 
+  if(!fdata) {
+    cerr << "Error opening data file " << fname << endl;
+    return false;
   }
+  fdata >> num; cout << "Number is:" << num << endl; 
+  Vertex *pvt = NULL;		// Vertex pointer. 
+  for(int i = 0; i < num; i++) { 
+    pvt = new (nothrow) Vertex(i);
+    v.push_back(pvt);
+    for(int j = 0; j < num; j++) {
+      fdata >> w;
+      if(w>0) {
+	cout << w;
+	Vertex *pnvt = new (nothrow) Vertex(j); 
+	if(!pnvt) {
+	  cerr << "Error while creating vertex object." << endl; 
+	  return false;
+	}
+	pnvt->setWeight(w);
+	pvt->setNext(pnvt); 
+	pvt = pnvt;
+      }	// if weight > 0.
+    } // for line. 
+    cout << endl;
+  } // while fdata. 
+  fdata.close();
+  return true;
+}
 
-  cout << "Please enter salary(int): ";
-  while (!(cin >> salary) || cin.get() != '\n') {
-    cin.clear();
-    cin.ignore(1000, '\n');
-    cout << "Incorrect entry! Please enter a number for salary: ";
+// Depth first traversal. 
+bool Graph::DFSTraversal(int id) {
+  cout << "Depth first traversal..." << endl;
+  visit(id);
+  Vertex *pv = v[id];
+  pv = pv->next();
+  while(pv != NULL) {
+    cout << pv->getWeight(); 
+    if(pv->next() != NULL) {cout << ",";}
+    pv = pv->next();
   }
-  
+  cout << endl; 
+  return true; 
 }
 
-// Constructor with vector. 
-Employee::Employee(const vector<string>& ed) {
-  last_name = ed.at(0);
-  first_name = ed.at(1);
-  eid = atoi(ed.at(2).c_str());
-  salary = atoi(ed.at(3).c_str());
-  department = ed.at(4);
-  phone_num = ed.at(5);
-  int pos_space = ed.at(6).find(" ", 0);
-  int len_str = ed.at(6).length();
-  office_num = ed.at(6).substr(0, pos_space);
-  office_building = ed.at(6).substr(pos_space+1, len_str - pos_space);
-  hire_date = ed.at(7);
-  email = ed.at(8);
+// Breadth first traversal. 
+bool Graph::BFSTraversal(int id) {
+  cout << "Breadth first traversal..." << endl;
+  return true; 
 }
 
-// Copy Constructor. 
-Employee::Employee(const Employee& e) {
-  last_name = e.getLastName();
-  first_name = e.getFirstName();
-  department = e.getDepartment();
-  phone_num = e.getPhoneNum();
-  office_building = e.getOfficeBld();
-  office_num = e.getOfficeNum();
-  hire_date = e.getHireDate();
-  email = e.getEmail();
-  eid = e.getEid();
-  salary = e.getSalary();
+// Dijkstra's shortest path algorithm.
+bool Graph::DijSP(int id) {
+  cout << "Dijkstra's Algorithm..." << endl; 
+  return true; 
 }
 
-// Assignment constructor. 
-Employee& Employee::operator=(const Employee& e) {
-  if(this == &e)
-    return *this; 
-  last_name = e.getLastName();
-  first_name = e.getFirstName();
-  department = e.getDepartment();
-  phone_num = e.getPhoneNum();
-  office_building = e.getOfficeBld();
-  office_num = e.getOfficeNum();
-  hire_date = e.getHireDate();
-  email = e.getEmail();
-  eid = e.getEid();
-  salary = e.getSalary(); 
-  return *this; 
+bool Graph::printAdjList() {
+  cout << "Printing adjacency list..." << endl; 
+  for(int i = 0; i < getNumVertices(); i++) {
+    Vertex *pv = v[i];
+    //cout << pv << endl; 
+    cout << pv->getId() << "->"; 
+    pv = pv->next();
+    while(pv != NULL) {
+      cout << pv->getWeight(); 
+      if(pv->next() != NULL) {cout << ",";}
+      pv = pv->next();
+    }
+    cout << endl; 
+  }
+  return true; 
 }
 
-// Following operators are comparing by lastname. 
-// Overloading the >= operator. 
-bool Employee::operator>=(Employee& e) {
-  return getLastName().compare(e.getLastName()) >= 0; 
+// Visit a vertex within the graph given by Id. 
+bool Graph::visit(int id) {
+  if((id > getNumVertices()) || (id < 0)) {
+    cerr << "Wrong id, please check again." << endl; 
+    return false;
+  }
+  Vertex *pv = v[id];
+  pv->visit(); 
+  return true; 
 }
-bool Employee::operator<=(Employee& e) {
-  return getLastName().compare(e.getLastName()) <= 0; 
-}
-bool Employee::operator>(Employee& e) {
-  return getLastName().compare(e.getLastName()) > 0; 
-}
-bool Employee::operator<(Employee& e) {
-  return getLastName().compare(e.getLastName()) < 0;
-}
-
-// Overloading the == operator. 
-bool Employee::operator==(Employee& e) {
-  return (last_name.compare(e.last_name)==0) && 
-    (first_name.compare(e.first_name)==0) &&
-    (department.compare(e.department)==0) &&
-    (phone_num.compare(e.phone_num)==0) &&
-    (office_building.compare(e.office_building)==0) &&
-    (office_num.compare(e.office_num)==0) &&
-    (hire_date.compare(e.hire_date)==0) &&
-    (email.compare(e.email)==0) &&
-    (eid == e.eid) &&
-    (salary == e.salary); 
-}
-
-// overloading >> operator for output.
-// with annotations. 
-ostream& operator>>(ostream& out, Employee& e) {
-  out << "Last:   " << e.getLastName() << endl
-      << "First:  " << e.getFirstName() << endl
-      << "EID:    " << e.getEid() << endl
-      << "Salary: " << e.getSalary() << endl
-      << "Dept:   " << e.getDepartment() << endl
-      << "Phone:  " << e.getPhoneNum() << endl 
-      << "Office: " << e.getOfficeNum() + " " + e.getOfficeBld() << endl
-      << "H-Date: " << e.getHireDate() << endl 
-      << "Email:  " << e.getEmail() << endl; 
-  return out; 
-}
-
-// overloading << also for output.
-// without annotations. 
-ostream& operator<<(ostream& out, Employee& e) {
-  out << e.getLastName() << endl
-      << e.getFirstName() << endl
-      << e.getEid() << endl
-      << e.getSalary() << endl
-      << e.getDepartment() << endl
-      << e.getPhoneNum() << endl 
-      << e.getOfficeNum() + " " + e.getOfficeBld() << endl
-      << e.getHireDate() << endl 
-      << e.getEmail() << endl; 
-  return out;
-}
-
-void Employee::print() {
-  cout << *this;
-}
-
--- fall-2010/data_structure/proj5/lab5.cpp
+++ fall-2010/data_structure/proj5/lab5.cpp
@@ -12,121 +12,75 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
-#include "Database.h"
+#include "Graph.h"
 using namespace std;
 
 // ---------------------------------------------------------------------
 // main() -- Test the function of insert, search, load data from file and
 //           save data to file. 
 // ---------------------------------------------------------------------
-void getCmd(char& choice); 
-bool execCmd(char choice);
-
-Database employeedb;		// the Employee database. 
+void renderUI(char& choice); 
+bool execCmd(char choice, Graph *graph);
 
 int main() {
+  Graph *graph = new (nothrow) Graph();
+  if(!graph) {
+    cout << "Error While creating Graph object. " << endl;
+    return 1; 
+  }
   char choice;			// User command choice. 
   do {
-    getCmd(choice);
-  } while(execCmd(choice));
+    renderUI(choice);
+  } while(execCmd(choice, graph));
   return 0;
 }
 
 // Prompt user to enter command 
 // The command char will be returned through a reference char. 
-void getCmd(char& choice) {
-  cout << "\nPlease Select Command From Following MENU:" << endl
-       << "(I/i)nsert new record" << endl
-       << "(D/d)elete record(by Eid)" << endl
-       << "(L/l)ast name search" << endl
-       << "(E/e)mployee id search" << endl
-       << "(S/s)ave database to a file" << endl
-       << "(R/r)ead database from a file" << endl
-       << "(P/p)rint ALL Employee records" << endl
-       << "(N/n)umber of Employee records" << endl
-       << "(Q/q)uit" << endl
+void renderUI(char& choice) {
+  cout << "\n------------Command Menu--------------" << endl
+       << "(A) Adjacency list load from file. " << endl
+       << "(D) Depth First Traversal " << endl
+       << "(B) Breadth First Traversal" << endl
+       << "(S) Shortest Path Dijkstra's Algorithm " << endl
+       << "(Q) Quit" << endl
        << endl << "Enter Choice: ";
   cin >> choice; 
 }
 
 // Execute user selected command. 
-// 'I/i' for inserting new Employee record to the list. 
-// 'D/d' for deleting employee record from list with eid.
-// 'L/l' for last name search throught the list. 
-// 'E/e' search by employee id. 
-// 'R/r' read Employee records from data file. 
-// 'S/s' save Employee data records into file. 
-// 'Q/q' exit this program. 
-// If a wrong command was entered, prompt the user to try again. 
-bool execCmd(char choice) {
-  string var; 
+bool execCmd(char choice, Graph *graph) {
   switch(choice) {
-  case 'I':
-  case 'i': {
-    employeedb.insertNewEmployee();
-    return true;
-  }
-  case 'L':
-  case 'l': {
-    cout << "Please enter last name: ";
-    cin >> var; 
-    if(var.compare("")!=0)
-      employeedb.findByLastname(var); 
+  case 'A':
+  case 'a': {
+    cout << "Loading graph data from file ......";
+    string fname = "graph.txt";
+    graph->loadFileToAdjList(fname);
+    graph->printAdjList();
+    cout << "done!" << endl;
     return true;
   }
   case 'D':
   case 'd': {
-    int eid; 
-    cout << "Enter employee ID(int) for delete: ";
-    while (!(cin >> eid) || cin.get() != '\n') {
-      cin.clear();
-      cin.ignore(1000, '\n');
-      cout << "Incorrect entry! Please enter a number for EID: ";
-    }
-    employeedb.deleteByEid(eid);
+    cout << "Depth First Graph Traversal. ";
+    graph->DFSTraversal(0);
+    cout << " done!" << endl;
     return true;
   }
-  case 'E':
-  case 'e': {
-    int eid; 
-    cout << "Enter employee ID(int) to search: ";
-    while (!(cin >> eid) || cin.get() != '\n') {
-      cin.clear();
-      cin.ignore(1000, '\n');
-      cout << "Incorrect entry! Please enter a number for EID: ";
-    }
-    employeedb.findByEid(eid);
-    //cout << "TODO: Search record by Eid." << endl;
+  case 'B':
+  case 'b': {
+    cout << "Breadth First Graph Traversal. ";
+    graph->BFSTraversal(0);
+    cout << " done!" << endl;
     return true;
-  }
-  case 'S':
-  case 's': {
-    employeedb.saveToFile(); 
-    return true;
-  }
-  case 'R':
-  case 'r': {
-    employeedb.loadFromFile();
-    return true;
-  }
-  case 'P':
-  case 'p': {
-    employeedb.printAll();
-    return true;
-  }
-  case 'N':
-  case 'n': {
-    cout << "Total Number of records: " << employeedb.size() << endl; 
-    return true; 
   }
   case 'Q':
   case 'q': {
-    cout << "Exiting program." << endl;
     return false;
   }
-  default: 
+  default:
     cout << "Wrong command: " << choice << ", please try again!" << endl;
-    cout << endl; 
-    return true;  
+    cout << endl;
+    return true;
   }
 }
